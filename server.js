cbc644e3ec94b7e6485db91f2243ccc9e4a8ca17
Wayne Edwards <waedwards42@yahoo.com>
2021-05-02 17:05:28 -0700

    added findById and filterByQuery functions

--- server.js
+++ server.js
@@ -37,30 +37,52 @@ function createNewNote(body, notesArray) {
     return note;
 }
 
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+}
+
+function filterbyQuery(query, notesArray) {
+    let filteredResults = notesArray;
+    if (query.title) {
+        filteredResults = filteredResults.filter(note => note.title === query.title);
+    }
+    return filteredResults;
+}
 
 
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
-app.get
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    if (req.query) {
+        results = filterbyQuery(req.query, results);
+    }
+    res.json(results);
+});
 
+app.get('/api/notes/:id', (req, res) => {
+    let result = findById(req.params.id, notes);
+    res.json(result);
+});
 
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const note = createNewNote(req.body, notes);
+    res.json(notes);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.put('/api/notes/:id', (req, res) => {
+    const note = updateNote(req.params.id, notes, req.body);
+    res.json(notes);
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
